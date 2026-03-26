285daf9b6f7ef6a0a28b11426fa48f32816121be
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2026-03-25 21:21:19 -0300

    feat: integrate the twitch with the bot

--- cmd/kitsune_bot/main.go
+++ cmd/kitsune_bot/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"KitsuneSemCalda/KitsuneBot/internal/db"
+	"KitsuneSemCalda/KitsuneBot/internal/twitch"
 	"log"
 )
 
 func main() {
+	config := twitch.LoadConfig()
+	client := twitch.NewClient(config)
+	registry := twitch.NewHandlerRegistry()
 	err := db.Setup("KitsuneBot.db")
 
 	if err != nil {
@@ -16,6 +20,19 @@ func main() {
 
 	if err != nil {
 		log.Printf("Error: %v", err)
+	}
+
+	defer db.DB.Close()
+
+	registry.Register(twitch.NewPingHandler())
+	registry.Register(twitch.NewStatsHandler())
+	registry.Register(twitch.NewHelpHandler(registry))
+
+	client.RegisterHandlers(registry)
+
+	log.Printf("Starting KitsuneBot for channel: %s", config.Channel)
+	if err := client.Connect(); err != nil {
+		log.Fatalf("Error connecting to Twitch: %v", err)
 	}
 
 }
